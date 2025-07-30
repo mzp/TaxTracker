6a7f3bdc48c7f721092f346f9752e2192214d592
mzp <me@mzp.jp>
2025-07-30 13:47:03 -0700

    Create Multi-platform project

---
+++ TaxTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaxTracker
+//
+//  Created by mzp on 2025/07/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaxTracker/TaxTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxTrackerApp.swift
+//  TaxTracker
+//
+//  Created by mzp on 2025/07/30.
+//
+
+import SwiftUI
+
+@main
+struct TaxTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

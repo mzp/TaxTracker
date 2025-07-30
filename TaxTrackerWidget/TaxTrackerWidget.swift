15611e92f9f317465cbd0e72a2eacb2a749b20cf
mzp <me@mzp.jp>
2025-07-30 14:31:13 -0700

    Add Tax Tracker Widget

---
+++ TaxTrackerWidget/TaxTrackerWidget.swift
@@ -0,0 +1,84 @@
+//
+//  TaxTrackerWidget.swift
+//  TaxTrackerWidget
+//
+//  Created by mzp on 2025/07/30.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct Provider: TimelineProvider {
+    func placeholder(in _: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in _: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in _: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+
+//    func relevances() async -> WidgetRelevances<Void> {
+//        // Generate a list containing the contexts this widget is relevant in.
+//    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct TaxTrackerWidgetEntryView: View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct TaxTrackerWidget: Widget {
+    let kind: String = "TaxTrackerWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                TaxTrackerWidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                TaxTrackerWidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    TaxTrackerWidget()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ TaxTrackerWidget/TaxTrackerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TaxTrackerWidgetBundle.swift
+//  TaxTrackerWidget
+//
+//  Created by mzp on 2025/07/30.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct TaxTrackerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TaxTrackerWidget()
+    }
+}
